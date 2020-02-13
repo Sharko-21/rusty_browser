c49b18b69f9e284913361e5f1c5a3848dc984396
yaroslavfilippov <yf@bssl.ru>
2020-02-13 14:21:42 +0300

    added DOM struct added search by ids and classes

--- src/DOM.rs
+++ src/DOM.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap};
 use std::rc::Rc;
-use std::cell::{RefCell};
+use std::cell::{RefCell, Ref};
 use std::ops::Add;
 use std::fmt;
 use std::fmt::{Formatter};
@@ -8,12 +8,13 @@ use std::fmt::{Formatter};
 #[derive(Debug)]
 pub struct Node {
     children: Vec<Rc<RefCell<Node>>>,
+    dom: Rc<RefCell<DOM>>,
     node_type: NodeType
 }
 
 impl Node {
-    pub fn new(parent: &mut DomElem, node_type: NodeType) -> Rc<RefCell<Node>> {
-        let node: Rc<RefCell<Node>> = Rc::new(RefCell::new(Node{children: vec![], node_type}));
+    pub fn new(parent: &mut DomElem, node_type: NodeType, dom: Rc<RefCell<DOM>>) -> Rc<RefCell<Node>> {
+        let node: Rc<RefCell<Node>> = Rc::new(RefCell::new(Node{children: vec![], dom, node_type}));
         match parent {
             DomElem::Node(parent) => parent.borrow_mut().children.push(node.clone()),
             DomElem::None => {}
@@ -42,8 +43,68 @@ impl Node {
         dom = dom + &format!("</{}>", &self.node_type.to_string());
         dom
     }
+
+    pub fn set_attr(&mut self, self_rc: Rc<RefCell<Node>>, key: String, value: String) {
+        if key == "class".to_string() {
+            let mut dom_ref = self.dom.borrow_mut();
+            match dom_ref.classes_node.get_mut(&key) {
+                None => {
+                    dom_ref.classes_node.insert(value.clone(), vec![self_rc.clone()]);
+                },
+                Some(vector) => {
+                    vector.push(self_rc.clone());
+                }
+            }
+        } else if key == "id".to_string() {
+            self.dom.borrow_mut().id_node.insert(value.clone(), self_rc);
+        }
+
+        match &mut self.node_type {
+            NodeType::Text(str) => {},
+            NodeType::Element(element) => {
+                element.attributes.insert(key, value);
+            },
+            _ => {}
+        };
+        return;
+    }
 }
 
+#[derive(Debug)]
+pub struct DOM {
+    root_node: DomElem,
+    classes_node: HashMap<String, Vec<Rc<RefCell<Node>>>>,
+    id_node: HashMap<String, Rc<RefCell<Node>>>
+}
+
+impl DOM {
+    pub fn new(root_node: DomElem, classes_node: HashMap<String, Vec<Rc<RefCell<Node>>>>, id_node: HashMap<String, Rc<RefCell<Node>>>) -> Rc<RefCell<DOM>>{
+        Rc::new(RefCell::new(DOM{root_node, classes_node, id_node}))
+    }
+
+    pub fn set_root(&mut self, root_node: DomElem) {
+        self.root_node = root_node;
+    }
+
+    pub fn find_by_class(&self, class: String) -> Option<&Vec<Rc<RefCell<Node>>>> {
+        self.classes_node.get(&class)
+    }
+
+    pub fn find_by_id(&self, id: String) -> Option<&Rc<RefCell<Node>>> {
+        self.id_node.get(&id)
+    }
+
+    pub fn print_tree(&self) -> String {
+        match &self.root_node {
+            DomElem::Node(node) => node.borrow().print_subtree_by_name(),
+            _ => {
+                String::new()
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum DomElem {
     Node(Rc<RefCell<Node>>),
     None
@@ -75,8 +136,14 @@ pub enum TAG {
 
 #[derive(Debug)]
 pub struct ElementData {
-    pub tag_name: TAG,
-    pub attributes: Attributes,
+    tag_name: TAG,
+    attributes: Attributes,
+}
+
+impl ElementData {
+    pub fn new(tag_name: TAG, attributes: Attributes) -> ElementData {
+        ElementData{tag_name, attributes}
+    }
 }
 
 impl fmt::Display for ElementData {
@@ -84,7 +151,7 @@ impl fmt::Display for ElementData {
         let mut print_body: String = String::new();
         print_body = print_body + &format!("{:?}", &self.tag_name);
         for (key, value) in &self.attributes {
-            print_body = print_body + &format!(" {}=\"{}\" ", key, value);
+            print_body = print_body + &format!(" {}=\"{}\"", key, value);
         }
         write!(f, "{}", print_body)
     }
